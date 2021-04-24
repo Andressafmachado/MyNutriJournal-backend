907e4942bef5773408d0d4ed06a0a7e082da2859
andressafmachado <andressamachado98@gmail.com>
2021-04-24 13:31:21 +0200

    improve seeds

--- seeders/20200309150002-some-doctors.js
+++ seeders/20200309150002-some-doctors.js
@@ -10,8 +10,8 @@ module.exports = {
       [
         {
           name: "Ana Jhonson",
-          email: "1@1.com",
-          password: bcrypt.hashSync("123", SALT_ROUNDS),
+          email: "ana@jhonson.com",
+          password: bcrypt.hashSync("ana", SALT_ROUNDS),
           image:
             "https://drturnersnutrition.com/wp-content/uploads/2019/07/diet_service_img-1.jpg",
           isDoctor: true,
@@ -21,9 +21,19 @@ module.exports = {
         {
           name: "Natalie Brady",
           email: "natalie@gmail.com",
-          password: bcrypt.hashSync("a", SALT_ROUNDS),
+          password: bcrypt.hashSync("natalie", SALT_ROUNDS),
           image:
-            "https://www.nataliebrady.co.nz/wp-content/uploads/2020/02/Auckland-nutritionist.jpg",
+            "https://res.cloudinary.com/andmachado/image/upload/v1618520399/xebmrx7qwarlkf2a1wwj.jpg",
+          isDoctor: true,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          name: "Marie Luna",
+          email: "marieluna@gmail.com",
+          password: bcrypt.hashSync("natalie", SALT_ROUNDS),
+          image:
+            "https://res.cloudinary.com/andmachado/image/upload/v1618520409/v1uo0z7qpb1tyhukuxuk.jpg",
           isDoctor: true,
           createdAt: new Date(),
           updatedAt: new Date(),
